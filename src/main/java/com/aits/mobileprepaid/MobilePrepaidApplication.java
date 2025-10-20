4e710ba501bc7b8a9347cdf039e4153789a68bc7
Bharath <bharthkamatham143@gmail.com>
2025-10-20 13:12:31 +0530

    Initial Commit Changes.

---
+++ src/main/java/com/aits/mobileprepaid/MobilePrepaidApplication.java
@@ -0,0 +1,13 @@
+package com.aits.mobileprepaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePrepaidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobilePrepaidApplication.class, args);
+    }
+
+}
