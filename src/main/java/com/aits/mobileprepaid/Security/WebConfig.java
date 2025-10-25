40a7e4929c974385361a3e9a1d34f9ee36c9630a
Bharath <bharthkamatham143@gmail.com>
2025-10-25 15:55:38 +0530

    Added Dockerfile for deployment.

--- src/main/java/com/aits/mobileprepaid/Security/WebConfig.java
+++ src/main/java/com/aits/mobileprepaid/Security/WebConfig.java
@@ -3,6 +3,7 @@ package com.aits.mobileprepaid.Security;
 import com.aits.mobileprepaid.Repositories.UserRepository;
 import com.aits.mobileprepaid.Services.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -29,13 +30,16 @@ public class WebConfig  {
     private final UserRepository userRepository;
     private final JWTFilter jwtFilter;
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
             http.csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:3000"));
+                    config.setAllowedOrigins(List.of(frontendUrl));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
